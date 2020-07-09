c4bd049235f5479f67d849f2595ac7f2594218e2
Márcio Gadelha <marciogadelha89@gmail.com>
2020-07-09 17:49:35 -0300

    Updates

---
+++ precache-manifest.8fa3de2a22a4156ea313bf260b298602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d2b634c8dd379f0b61b8a1a6042795",
+    "url": "/crud-reactjs/index.html"
+  },
+  {
+    "revision": "ed2f8464bd9338edf4dd",
+    "url": "/crud-reactjs/static/css/main.f5077e9f.chunk.css"
+  },
+  {
+    "revision": "bb42f02f81ba6b37bab6",
+    "url": "/crud-reactjs/static/js/2.5afd00f6.chunk.js"
+  },
+  {
+    "revision": "ed2f8464bd9338edf4dd",
+    "url": "/crud-reactjs/static/js/main.b4dd83d7.chunk.js"
+  },
+  {
+    "revision": "40a67860023385eaf4a9",
+    "url": "/crud-reactjs/static/js/runtime~main.cc2c28c8.js"
+  }
+]);
